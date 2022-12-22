5e0e673af261a0a3920860c30f308d6b3acdc0a1
Israel Yusuf <sentrytim10@gmail.com>
2022-12-22 08:11:35 +0100

    Renteasy
    Homepage Re-edited

---
+++ script.js
@@ -0,0 +1,17 @@
+function func1()  {
+    var x=document.getElementById("freq1");
+    if(x.style.display==="none"){
+        x.style.display="block";
+    }else{
+        x.style.display="none";
+    }
+};
+
+function func2()  {
+    var x=document.getElementById("freq2");
+    if(x.style.display==="none"){
+        x.style.display="block";
+    }else{
+        x.style.display="none";
+    }
+};
